ab56bd2e8414954068ed9bcd9b37b080374f5cfe
GGGSergeyJS <gggsergeyjs@gmail.com>
2023-11-30 17:36:06 +0300

    add dynamic routing page

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,37 +1,20 @@
 import React from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
-import About from "../pages/About";
-import Posts from "../pages/Posts";
-import Error from "../pages/Error";
-import PostIdPage from "../pages/PostIdPage";
-// import { routes } from "../router/routes";
+import { Route, Routes } from "react-router-dom";
+import { routes } from "../router/routes";
 
 const AppRouter = () => {
     return (
         <Routes>
-            <Route path="/about" element={<About />} />
-            <Route path="/posts" element={<Posts />} />
-            <Route path="/posts/:id" element={<PostIdPage />} />
-            <Route path="/error" element={<Error />} />
-            <Route path="/" element={<Navigate replace to="/posts" />} />
-            <Route path="/*" element={<Navigate replace to="/error" />} />
+            {routes.map((route, index) => (
+                <Route
+                    key={index}
+                    path={route.path}
+                    element={route.element}
+                    exact={route.exact}
+                />
+            ))}
         </Routes>
     );
 };
-
-// const AppRouter = () => {
-//     return (
-//         <Routes>
-//             {routes.map((route, index) => (
-//                 <Route
-//                     key={index}
-//                     path={route.path}
-//                     element={route.element}
-//                     exact={route.exact}
-//                 />
-//             ))}
-//         </Routes>
-//     );
-// };
 
 export default AppRouter;
